c35e104b95fb2a7a4ddc911e523969d7458390b9
Dobry Nikolov <dobry989@gmail.com>
2026-06-18 10:47:21 +0300

    chore: filter stale image alerts

--- workloads/namespace/monitoring/apps/version-checker/prometheusrule.nix
+++ workloads/namespace/monitoring/apps/version-checker/prometheusrule.nix
@@ -17,15 +17,20 @@
             # Fires only after 24h of continuous drift to debounce transient
             # registry hiccups during the rolling window.
             expr = ''
-              version_checker_is_latest_version{
-                image!="rancher/klipper-helm",
-                image!="postgres",
-                image!="docker.io/bitnamilegacy/redis",
-                image!="docker.io/bitnamilegacy/redis-exporter",
-                latest_version!~"^(sha-|snapshot|unstable|nightly|develop|dev|master|pr|latest-snapshot).*$",
-                latest_version!~".*(-dev|-pr|_beta).*$",
-                latest_version!~"^[0-9]{8,}$"
-              } == 0
+              (
+                version_checker_is_latest_version{
+                  image!="rancher/klipper-helm",
+                  image!="rancher/mirrored-coredns-coredns",
+                  image!="postgres",
+                  image!="docker.io/bitnamilegacy/redis",
+                  image!="docker.io/bitnamilegacy/redis-exporter",
+                  latest_version!~"^(sha-|snapshot|unstable|nightly|develop|dev|master|pr|latest-snapshot).*$",
+                  latest_version!~".*(-dev|-pr|_beta).*$",
+                  latest_version!~"^[0-9]{8,}$"
+                } == 0
+              )
+              and on(namespace, pod)
+              (kube_pod_status_phase{phase="Running"} == 1)
             '';
             "for" = "24h";
             labels = {
